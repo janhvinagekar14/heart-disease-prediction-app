bb37d2f09ea0ee2ada4d20a2249dde605678c013
janhvinagekar14 <155978365+janhvinagekar14@users.noreply.github.com>
2026-04-20 19:54:01 +0530

    Refactor input fields for heart disease prediction

--- app.py
+++ app.py
@@ -12,23 +12,22 @@ st.title("Heart Disease Prediction")
 # Inputs (MATCH EXACT ORDER)
 
 age = st.number_input("Age")
-restingbp = st.number_input("Resting Blood Pressure")
-chol = st.number_input("Cholesterol")
+
+
 fastingbs = st.selectbox("Fasting Blood Sugar > 120", [0,1])
 maxhr = st.number_input("Max Heart Rate")
-oldpeak = st.number_input("Oldpeak")
+
 
 sex = st.selectbox("Sex (0=Female, 1=Male)", [0,1])
 
-cp = st.selectbox("Chest Pain Type (encoded)", [0,1,2,3])
-restecg = st.selectbox("Resting ECG (encoded)", [0,1,2])
+
 exercise_angina = st.selectbox("Exercise Angina (0=No,1=Yes)", [0,1])
 st_slope = st.selectbox("ST Slope (0=Up,1=Flat,2=Down)", [0,1,2])
 
 # Prediction
 if st.button("Predict"):
-    data = np.array([[age, restingbp, chol, fastingbs, maxhr, oldpeak,
-                      sex, cp, restecg, exercise_angina, st_slope]])
+    data = np.array([[age,  fastingbs, maxhr, 
+                      sex,  exercise_angina, st_slope]])
 
     prediction = model.predict(data)
     prob = model.predict_proba(data)[0][1]
